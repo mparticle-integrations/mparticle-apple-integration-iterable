114a0e8c70aed3a34fde18f6fc07b24119ca5d1d
Steven Lehrburger <lehrburger@gmail.com>
2017-06-30 17:29:01 -0400

    Remove duplicated URL regex to check for Iterable links
    Also, add a missing } at the end of the file.

--- mParticle-iterable/IterableMPHelper.m
+++ mParticle-iterable/IterableMPHelper.m
@@ -20,19 +20,24 @@
 
 +(void) getAndTrackDeeplink:(NSURL *)webpageURL callbackBlock:(ITEActionBlock)callbackBlock
 {
-    NSRegularExpression *regex = [NSRegularExpression regularExpressionWithPattern:ITBL_DEEPLINK_IDENTIFIER options:0 error:NULL];
-    NSString *urlString = webpageURL.absoluteString;
-    NSTextCheckingResult *match = [regex firstMatchInString:urlString options:0 range:NSMakeRange(0, [urlString length])];
-    
-    if (match == NULL) {
-        callbackBlock(webpageURL.absoluteString);
-    } else {
+    if ([self isIterableDeeplink:webpageURL]) {
         NSURLSessionDataTask *trackAndRedirectTask = [[NSURLSession sharedSession]
                                                       dataTaskWithURL:webpageURL completionHandler:^(NSData *data, NSURLResponse *response, NSError *error) {
                                                           callbackBlock(response.URL.absoluteString);
                                                       }];
         [trackAndRedirectTask resume];
+    } else {
+        callbackBlock(webpageURL.absoluteString);
     }
+}
+
++(BOOL) isIterableDeeplink:(NSURL *)webpageURL
+{
+    NSRegularExpression *regex = [NSRegularExpression regularExpressionWithPattern:ITBL_DEEPLINK_IDENTIFIER options:0 error:NULL];
+    NSString *urlString = webpageURL.absoluteString;
+    NSTextCheckingResult *match = [regex firstMatchInString:urlString options:0 range:NSMakeRange(0, [urlString length])];
+    
+    return match != NULL;
 }
 
 @end
