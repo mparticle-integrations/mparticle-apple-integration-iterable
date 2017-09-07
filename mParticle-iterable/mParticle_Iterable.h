ad2c696c3a809170a499fd80c2594f68a7c74893
Peter Jenkins <pjenkins@mparticle.com>
2017-09-07 12:25:29 -0400

    Add support for Carthage

---
+++ mParticle-iterable/mParticle_Iterable.h
@@ -0,0 +1,11 @@
+#import <UIKit/UIKit.h>
+
+//! Project version number for mParticle-Iterable.
+FOUNDATION_EXPORT double mParticle_IterableVersionNumber;
+
+//! Project version string for mParticle-Iterable.
+FOUNDATION_EXPORT const unsigned char mParticle_IterableVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <mParticle_Iterable/PublicHeader.h>
+
+#import <mParticle_Iterable/MPKitIterable.h>
