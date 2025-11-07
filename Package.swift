2f4d22ddcc0247cd7c47740a1fb684aa62c89a6a
Mo Mustafa <96316894+mmustafa-tse@users.noreply.github.com>
2025-11-07 08:43:27 -0800

    feat: Add no location target to Iterable kit (#22)
    * feat: Add no location target to Iterable kit
    
    * add mPIterableNoLocation product
    
    ---------
    
    Co-authored-by: Mohammed Mustafa <mohammedmustafa@Mohammeds-MacBook-Pro.local>

--- Package.swift
+++ Package.swift
@@ -3,34 +3,55 @@
 
 import PackageDescription
 
+let noLocation = "-NoLocation"
+let mpIterable = "mParticle-Iterable"
+let mpIterableNoLocation = mpIterable + noLocation
+
+// MARK: - External Packages
+let mpSDK = "mParticle-Apple-SDK"
+let mpSDKNoLocation = mpSDK + noLocation
+let iterableSDK = "IterableSDK"
+
 let package = Package(
-    name: "mParticle-Iterable",
+    name: mpIterable,
     platforms: [ .iOS(.v11) ],
     products: [
-        .library(
-            name: "mParticle-Iterable",
-            targets: ["mParticle-Iterable"]),
+        .library(name: mpIterable, targets: [mpIterable]),
+        .library(name: mpIterableNoLocation, targets: [mpIterableNoLocation])
     ],
     dependencies: [
-      .package(name: "mParticle-Apple-SDK",
+      .package(name: mpSDK,
                url: "https://github.com/mParticle/mparticle-apple-sdk",
                .upToNextMajor(from: "8.19.0")),
-      .package(name: "IterableSDK",
+      .package(name: iterableSDK,
                url: "https://github.com/Iterable/swift-sdk",
                .upToNextMajor(from: "6.5.2")),
     ],
     targets: [
         .target(
-            name: "mParticle-Iterable",
+            name: mpIterable,
             dependencies: [
-                .product(name: "mParticle-Apple-SDK", package: "mParticle-Apple-SDK"),
-                .product(name: "IterableSDK", package: "IterableSDK"),
-                .product(name: "IterableAppExtensions", package: "IterableSDK")
+                .product(name: mpSDK, package: mpSDK),
+                .product(name: iterableSDK, package: iterableSDK),
+                .product(name: "IterableAppExtensions", package: iterableSDK)
             ],
-            path: "mParticle-Iterable",
+            path: "mParticle-iterable",
             exclude: ["Info.plist"],
             resources: [.process("PrivacyInfo.xcprivacy")],
             publicHeadersPath: "."
+        ),
+        .target(
+            name: mpIterableNoLocation,
+            dependencies: [
+                .product(name: mpSDKNoLocation, package: mpSDK),
+                .product(name: iterableSDK, package: iterableSDK),
+                .product(name: "IterableAppExtensions", package: iterableSDK)
+            ],
+            path: "mParticle-iterable-NoLocation",
+            exclude: ["Info.plist"],
+            resources: [.process("PrivacyInfo.xcprivacy")],
+            publicHeadersPath: ".",
+            cSettings: [.define("MP_NO_LOCATION")]
         ),
     ]
 )
